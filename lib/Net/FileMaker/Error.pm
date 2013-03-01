5a73e3656244916386d59556aecd530edc1340e1
Squeeks <privacymyass@gmail.com>
2013-03-01 16:33:09 +0000

    Minor updates to documentation

--- lib/Net/FileMaker/Error.pm
+++ lib/Net/FileMaker/Error.pm
@@ -10,22 +10,27 @@ Net::FileMaker::Error - Error strings and codes
 
 =head1 SYNOPSIS
 
-Classes within this namespace store the applicable error codes, and the localised strings for them. You don't call these modules yourself directly. To enable them, the key "error" must be defined as an ISO 639 short code when you construct your objects with L<Net::FileMaker> or a subclass that handles individual interface types like so:
+Classes within this namespace store the applicable error codes, and the
+localised strings for them. You don't call these modules yourself directly. To
+enable them, the key "error" must be defined as an ISO 639 short code when you
+construct your objects with L<Net::FileMaker> or a subclass that handles
+individual interface types like so:
 
     use Net::FileMaker;
     my $fms = Net::FileMaker->new(host => $host, error => 'en');
 
 =head1 LANGUAGES SUPPORTED
 
-English, Japanese and German are presently supported with the aims of covering all languages documented by FileMaker in the future.
+English, Italian, Japanese and German are presently supported with the aims of
+covering all languages documented by FileMaker in the future.
 
 =cut
 
 # new( lang => $lang, type => $type)
 #
-# Creates a new object. There is no inheritance in the Error classes, mearly factory objects.
-# Both the language (in ISO 639) and type (XML/XSLT/IWP etc) needs be defined. Returns undef if
-# fails to load the strings.
+# Creates a new object. There is no inheritance in the Error classes, mearly
+# factory objects.  Both the language (in ISO 639) and type (XML/XSLT/IWP etc)
+# needs be defined. Returns undef if fails to load the strings.
 sub new
 {
     my($class, %args) = @_;
